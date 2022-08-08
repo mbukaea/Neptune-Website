2825bc463c2795343508f7dd1fde9098c7a5867d
Matthew <matthew.barton@ukaea.uk>
2022-08-08 10:22:28 +0100

    Updated readme, run.sh and removed unneeded files

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
-rm main.aux main.log main.out main_html.aux main_html.log *.html
+#Removes files from previous runs
+rm main.aux main.log main.out main_html.aux main_html.log *.html *lwarp*
+#Produces pdf version of website contents, alongside lwarp files needed
+#to create the website
 pdflatex main
+#Compiles website using lwarp package
 lwarpmk html
-gsed -i -e 's,<section class="textbody">,<h1>main</h1>,g' main.html
-gsed -n 245,484p Engineering-Requirements-Baseline.html  >> sidebar.txt
- gsed -i -e '/<div class="bodywithoutsidetoc">/r sidebar.txt' main.html
-for file in *.html; do file2="${file/-/ }";file3="${file2/-/ }" ; gsed -i "s,<h1>main</h1>,<h1>${file3::-5}</h1>,g" ${file}; done 
+#Changes headers of each html file to contain name of section + adds sidebar to main page
+sed -i -e 's,<section class="textbody">,<h1>main</h1>,g' main.html
+sed -n 245,484p Engineering-Requirements-Baseline.html  >> sidebar.txt
+sed -i -e '/<div class="bodywithoutsidetoc">/r sidebar.txt' main.html
+for file in *.html; do file2="${file/-/ }";file3="${file2/-/ }" ; sed -i "s,<h1>main</h1>,<h1>${file3::-5}</h1>,g" ${file}; done 
 
