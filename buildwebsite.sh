490aaf683ea228410d29f6e72a565d5dfc27b346
Matthew <matthew.barton@ukaea.uk>
2022-09-12 11:33:33 +0100

    Drop down menu replacement for sidebar table of contents

--- buildwebsite.sh
+++ buildwebsite.sh
@@ -18,6 +18,7 @@ function buildwebsite() {
   if [ "$1" != "PDF" ]
   then
   lwarpmk html
+  rm main_html.html    
   #Changes headers of each html file to contain name of section + adds sidebar to main page
   sed -i -e 's,<section class="textbody">,<h1>Neptune</h1>,g' main.html
   sed -i -e '/<div class="center">/r link.txt' main.html
@@ -25,13 +26,31 @@ function buildwebsite() {
   test=$(grep -r -n 'bodyandsidetoc' ./Engineering-Requirements-Baseline.html | cut -f1 -d:)
   test2=$(grep -r -n 'bodycontainer' ./Engineering-Requirements-Baseline.html | cut -f1 -d:)
   test3=$( expr $test2 - 3)
-  sed -n "${test},${test3}p" Engineering-Requirements-Baseline.html >> sidebar.txt
-  sed -i -e '/<div class="bodywithoutsidetoc">/r sidebar.txt' main.html
+  test4=$( expr $test + 1)
+  sed -n "${test4},${test3}p" Engineering-Requirements-Baseline.html >> sidebar.txt
+  cat menustart.txt sidebar.txt menuend.txt > sidebar2.txt
+  test=$(grep -r -n '<div class="sidetoctitle">' sidebar2.txt | cut -f1 -d:)
+  test2=$( expr $test + 9)
+  sed -i -e "$test,$test2 d" sidebar2.txt
+  sed -i -e '/<div class="bodywithoutsidetoc">/r sidebar2.txt' main.html
+  sed -i -e 's/<div class="bodywithoutsidetoc">/ /g' main.html
+  mv ./main.html ../
+  test=$(grep -r -n '<div class="bodyandsidetoc">' ./Videos.html | cut -f1 -d: )
+  test2=$( expr $test + 1)
+  test=$(grep -r -n '<main class="bodycontainer">' ./Videos.html | cut -f1 -d: )
+  test3=$( expr $test - 1)
+  sed -i -e "$test2,$test3 d" *.html
+  sed -i -e '$d' sidebar2.txt
+  sed -i -e '/<div class="bodyandsidetoc">/r sidebar2.txt' *.html
+  mv ../main.html ./
+  test=$(grep -r -n 'Download' ./main.html  | tail -n 1 | cut -f1 -d:)
+  test2=$( expr $test - 1)
+  sed -i -e "$test,$test2 d" main.html
   for file in *.html; do file2="${file/-/ }";file3="${file2/-/ }" ; sed -i "s,<h1>main</h1>,<h1>${file3::-5}</h1>,g" ${file}; done
   test4=$(grep -r -n '<h3 id' ./main.html | cut -f1 -d:)
   test5=$(grep -r -n '</nav>' ./main.html | tail -n 1 | cut -f1 -d:)
   sed -i -e "$test4,$test5 d" main.html
-  sed -i -e '693,694d' main.html
+#  sed -i -e '693,694d' main.html
   cp main.html index.html
   #Resize images on webpages
   sed -i -e '/videoinsert/r videos.txt' Videos.html
